b0b731faf1861244ddc2511f9188bfc899b9d305
juan <juancvillefer@gmail.com>
2024-05-31 15:49:03 -0400

    exercise router

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,11 @@
 "use client";
 import Image from "next/image";
 import styles from "./page.module.css";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
 export default function Home() {
-  
   const [user, setUser] = useState("Levis");
   const data = "Pieman";
   const update = () => {
@@ -12,9 +13,10 @@ export default function Home() {
   };
 
   useEffect(() => {
-    console.log("prueba del effect")
+    console.log("prueba del effect");
   }, [user]);
 
+  const router = useRouter();
   return (
     <main className={styles.main}>
       {/* <Page name="first" />
@@ -22,12 +24,13 @@ export default function Home() {
       <Page name="third" />
       <Page name="fourth" /> */}
 
-      <h1>Hello Word {user}</h1>
-      <button onClick={() => update()}>Click here</button>
+      <h1>Hello World {user}</h1>
+      <button onClick={() => router.push("/blog")}>Click here</button>
+      {/* <Link href="">go to Blogs</Link> */}
     </main>
   );
 }
 
 const Page = (props) => {
-  return <div>{props.name} page</div>
+  return <div>{props.name} page</div>;
 }
