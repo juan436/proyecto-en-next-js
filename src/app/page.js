aeba5b72bdc09cc5bc1fae8bbf3d95170737b2db
juan <juancvillefer@gmail.com>
2024-05-31 15:07:57 -0400

    practice useState and useEffect

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,20 @@
 "use client";
 import Image from "next/image";
 import styles from "./page.module.css";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  const color = (picker) => {
-    alert(picker);
+  
+  const [user, setUser] = useState("Levis");
+  const data = "Pieman";
+  const update = () => {
+    setUser("Carlos");
   };
+
+  useEffect(() => {
+    console.log("prueba del effect")
+  }, [user]);
+
   return (
     <main className={styles.main}>
       {/* <Page name="first" />
@@ -13,8 +22,8 @@ export default function Home() {
       <Page name="third" />
       <Page name="fourth" /> */}
 
-      <h1>Hello Word</h1>
-      <button onClick={() => color("yellow")} >Click here</button>
+      <h1>Hello Word {user}</h1>
+      <button onClick={() => update()}>Click here</button>
     </main>
   );
 }
