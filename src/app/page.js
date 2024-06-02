6f3266eb3588a2b70184a5ec418647746cfcc980
juan <juancvillefer@gmail.com>
2024-06-02 16:04:43 -0400

    practice with navigation dinamic

--- src/app/page.js
+++ src/app/page.js
@@ -25,7 +25,7 @@ export default function Home() {
       {/* <Page name="first" />
       <Page name="second" />
       <Page name="third" />
-      <Page name="fourth" /> */}s
+      <Page name="fourth" /> */}
 
       <h1>Hello World {user}</h1>
       <button onClick={() => navigation("newblog")}>Click here blog-new</button>
